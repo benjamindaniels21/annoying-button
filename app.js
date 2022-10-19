22757d7e91d18b50414745fddcafc0bfa55fc5f0
Ben Daniels <benjamindaniels21@gmail.com>
2022-10-19 09:47:21 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+btn = document.querySelector("button");
+
+btn.addEventListener("mouseover", function () {
+  console.log("moused");
+  const height = Math.floor(Math.random() * window.innerHeight);
+  const width = Math.floor(Math.random() * window.innerWidth);
+  btn.style.left = `${width}px`;
+  btn.style.top = `${height}px`;
+});
+
+btn.addEventListener("click", function () {
+  btn.innerText = "You got me!";
+  document.body.style.backgroundColor = "green";
+});
